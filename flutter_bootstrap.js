425932658d4d9db21790d4aff056e40795db66d6
ondabii <ondabii@kakao.com>
2025-12-05 00:22:39 +0900

    before sleep

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3045799645"
+    serviceWorkerVersion: "2091838016"
   }
 });
